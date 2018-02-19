f54a7bf0986ef339d5bee7ea9f43fc357c63e0f3
Diego Felix <diegoflx.oliveira@gmail.com>
2018-02-18 22:09:16 -0300

    fix registration fields

--- resources/views/auth/completion.blade.php
+++ resources/views/auth/completion.blade.php
@@ -15,7 +15,7 @@
                             <label for="cpf" class="col-md-4 col-form-label text-md-right">CPF</label>
 
                             <div class="col-md-6">
-                                <input id="cpf" type="text" class="form-control{{ $errors->has('cpf') ? ' is-invalid' : '' }}" name="name" value="{{ old('cpf') }}" required autofocus>
+                                <input id="cpf" type="text" class="form-control{{ $errors->has('cpf') ? ' is-invalid' : '' }}" name="cpf" value="{{ old('cpf') }}" required autofocus>
 
                                 @if ($errors->has('cpf'))
                                     <span class="invalid-feedback">
@@ -29,7 +29,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">Telefone</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="name" value="{{ old('phone') }}" required>
+                                <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required>
 
                                 @if ($errors->has('phone'))
                                     <span class="invalid-feedback">
@@ -43,7 +43,7 @@
                             <label for="postal_code" class="col-md-4 col-form-label text-md-right">CEP</label>
 
                             <div class="col-md-6">
-                                <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="name" value="{{ old('postal_code') }}" required>
+                                <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="postal_code" value="{{ old('postal_code') }}" required>
 
                                 @if ($errors->has('postal_code'))
                                     <span class="invalid-feedback">
@@ -57,7 +57,7 @@
                             <label for="street" class="col-md-4 col-form-label text-md-right">Rua</label>
 
                             <div class="col-md-6">
-                                <input id="street" type="text" class="form-control{{ $errors->has('street') ? ' is-invalid' : '' }}" name="name" value="{{ old('street') }}" required>
+                                <input id="street" type="text" class="form-control{{ $errors->has('street') ? ' is-invalid' : '' }}" name="street" value="{{ old('street') }}" required>
 
                                 @if ($errors->has('street'))
                                     <span class="invalid-feedback">
@@ -71,7 +71,7 @@
                             <label for="number" class="col-md-4 col-form-label text-md-right">Número</label>
 
                             <div class="col-md-6">
-                                <input id="number" type="text" class="form-control{{ $errors->has('number') ? ' is-invalid' : '' }}" name="name" value="{{ old('number') }}" required>
+                                <input id="number" type="text" class="form-control{{ $errors->has('number') ? ' is-invalid' : '' }}" name="number" value="{{ old('number') }}" required>
 
                                 @if ($errors->has('number'))
                                     <span class="invalid-feedback">
@@ -85,7 +85,7 @@
                             <label for="complement" class="col-md-4 col-form-label text-md-right">Complemento</label>
 
                             <div class="col-md-6">
-                                <input id="complement" type="text" class="form-control" name="name" value="{{ old('complement') }}">
+                                <input id="complement" type="text" class="form-control" name="complement" value="{{ old('complement') }}">
                             </div>
                         </div>
 
@@ -93,7 +93,7 @@
                             <label for="district" class="col-md-4 col-form-label text-md-right">Bairro</label>
 
                             <div class="col-md-6">
-                                <input id="district" type="text" class="form-control{{ $errors->has('district') ? ' is-invalid' : '' }}" name="name" value="{{ old('district') }}" required>
+                                <input id="district" type="text" class="form-control{{ $errors->has('district') ? ' is-invalid' : '' }}" name="district" value="{{ old('district') }}" required>
 
                                 @if ($errors->has('district'))
                                     <span class="invalid-feedback">
@@ -108,7 +108,7 @@
                             <label for="city" class="col-md-4 col-form-label text-md-right">Cidade</label>
 
                             <div class="col-md-6">
-                                <input id="city" type="text" class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }}" name="name" value="{{ old('city') }}" required>
+                                <input id="city" type="text" class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }}" name="city" value="{{ old('city') }}" required>
 
                                 @if ($errors->has('city'))
                                     <span class="invalid-feedback">
@@ -122,7 +122,7 @@
                             <label for="state" class="col-md-4 col-form-label text-md-right">Estado</label>
 
                             <div class="col-md-6">
-                                <input id="state" type="text" class="form-control{{ $errors->has('state') ? ' is-invalid' : '' }}" name="name" value="{{ old('state') }}" required>
+                                <input id="state" type="text" class="form-control{{ $errors->has('state') ? ' is-invalid' : '' }}" name="state" value="{{ old('state') }}" required>
 
                                 @if ($errors->has('state'))
                                     <span class="invalid-feedback">
