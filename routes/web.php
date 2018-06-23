2dd0703ec10bc27502527e75d000cbb51b43ed86
Diego Felix <diegoflx.oliveira@gmail.com>
2018-06-23 13:42:12 -0300

    little refactor on routes file

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,14 +8,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('completar-cadastro', [
-    'middleware' => 'auth',
-    'as' => 'completeRegistration',
-    'uses' => 'Auth\CompleteRegistrationController@create'
-]);
-
-Route::post('completar-cadastro', [
-    'middleware' => 'auth',
-    'as' => 'completeRegistration',
-    'uses' => 'Auth\CompleteRegistrationController@store'
-]);
+Route::group(['middleware' => 'auth', 'as' => 'completeRegistration'], function () {
+    Route::get('completar-cadastro', 'Auth\CompleteRegistrationController@create');
+    Route::post('completar-cadastro', 'Auth\CompleteRegistrationController@store');
+});
