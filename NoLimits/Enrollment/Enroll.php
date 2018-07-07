d1efcd267d6b684b885809831919749919972408
Diego Felix <diegoflx.oliveira@gmail.com>
2018-07-06 23:37:00 -0300

    generate custom id for enrolls

--- NoLimits/Enrollment/Enroll.php
+++ NoLimits/Enrollment/Enroll.php
@@ -22,4 +22,33 @@ class Enroll extends Model
     {
         return $this->embedsMany(Competition::class, 'competitions');
     }
+
+    public function save(bool $force = false)
+    {
+        if (!$this->_id) {
+            $this->_id = $this->generateId();
+        }
+
+        parent::save($force);
+    }
+
+    private function generateId(): string
+    {
+        $date = $this->generateIdBasedOnTimestamp();
+        $rand = $this->generateRandomNumber();
+
+        return strtoupper("NL{$date}{$rand}");
+    }
+
+    private function generateIdBasedOnTimestamp(): string
+    {
+        return str_pad(base_convert(date('U'), 10, 20), 8, '0', STR_PAD_LEFT);
+    }
+
+    private function generateRandomNumber(): string
+    {
+        $rand = substr(rand(), 0, 3);
+
+        return str_pad(base_convert($rand, 10, 20), 4, '0', STR_PAD_LEFT);
+    }
 }
